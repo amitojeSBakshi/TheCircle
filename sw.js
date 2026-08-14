3c20477decf3492972d822282ec3da0921097168
Amitoje Singh Bakshi <amitojeSBakshi@users.noreply.github.com>
2026-08-15 00:04:59 +0530

    Publish 20260814-183459-5772

---
+++ sw.js
@@ -0,0 +1,33 @@
+﻿/* Circle hub - build 20260814-183459-5772 */
+var CACHE = "circle-hub-20260814-183459-5772";
+var ASSETS = ["./", "./index.html", "./manifest.json",
+              "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).catch(function(){}));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){ if (k !== CACHE) return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+
+self.addEventListener("fetch", function(e){
+  var req = e.request;
+  if (req.method !== "GET") return;
+  if (req.mode === "navigate" || (req.headers.get("accept") || "").indexOf("text/html") !== -1) {
+    e.respondWith(
+      fetch(req).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(req, copy); }).catch(function(){});
+        return res;
+      }).catch(function(){
+        return caches.match(req).then(function(hit){ return hit || caches.match("./index.html"); });
+      })
+    );
+    return;
+  }
+  e.respondWith(caches.match(req).then(function(hit){ return hit || fetch(req); }));
+});
